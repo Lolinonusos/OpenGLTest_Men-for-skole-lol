38700212401a56df32bddac095cc60ddc2fe6aab
TaisaKiarkhi <89452998+TaisaKiarkhi@users.noreply.github.com>
2023-02-04 19:34:33 +0100

    tetragon_changes

--- OpenGLTest/tetrahedon.cpp
+++ OpenGLTest/tetrahedon.cpp
@@ -1,13 +1,14 @@
 #include "tetrahedon.h"
 #include <vertex.h>
 
+
 Tetrahedon::Tetrahedon()
 {
-	Vertex_data.push_back(Vertex{ 1.0f, 1.0f, 0.0f,  0.0f ,0.0f, 0.0f });
+	Vertex_data.push_back(Vertex{ 0.0f, 0.0f, 0.0f,  0.0f ,0.0f, 0.0f });
+	Vertex_data.push_back(Vertex{ 0.0f, 0.0f, 1.0f,  0.0f ,0.0f, 0.0f });
 	Vertex_data.push_back(Vertex{ 0.0f, 1.0f, 0.0f,  0.0f ,0.0f, 0.0f });
 	Vertex_data.push_back(Vertex{ 1.0f, 0.0f, 0.0f,  0.0f ,0.0f, 0.0f });
-	Vertex_data.push_back(Vertex{ 0.0f, 0.0f, 0.0f,  0.0f ,0.0f, 0.0f });
-	Vertex_data.push_back(Vertex{ 0.5f, 0.5f, 1.0f,  0.0f ,0.0f, 0.0f });
+	//Vertex_data.push_back(Vertex{ 0.5f, 0.5f, 1.0f,  0.0f ,0.0f, 0.0f });
 
 }
 
@@ -17,19 +18,24 @@ Tetrahedon::~Tetrahedon()
 
 void Tetrahedon::init(int inMatrixUniform)
 {
-	glGenBuffers(1, &BufferID);
-	glBindBuffer(GL_ARRAY_BUFFER, BufferID);
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex_data), &Vertex_data, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, sizeof(Vertex_data), GL_FLOAT, GL_FALSE, 0, 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
+	glGenVertexArray(1, &VAO);
+	glBindVertexArray(&VAO);
 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (char*)(sizeof(Vertex_data) * 5);
+
+	glEnableAttribArray(1);
 }
 
 void Tetrahedon::draw()
 {
-	glViewport(0, 0, 500, 500);
-	glDrawArrays(GL_TRIANGLES, 0, 5);
+	glUniformMatrix4fv(matrixUniform, 1, GL_FALSE, glm::value_ptr(matrix));
+	glDrawArrays(GL_TRIANGLES, 0, 4);
 
 }
